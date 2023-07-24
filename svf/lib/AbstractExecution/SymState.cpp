76ec303db16fcd72ea5f99d0cf6d8c979fc6abfe
jumormt <jumormt@163.com>
2023-07-24 21:08:50 +1000

    typestate: string -> enum class

--- svf/lib/AbstractExecution/SymState.cpp
+++ svf/lib/AbstractExecution/SymState.cpp
@@ -29,6 +29,21 @@
 
 using namespace SVF;
 
+Map<std::string, TypeState> TypeStateParser::_typeMap = {
+    {"$uninit",   TypeState::Uinit},
+    {"$error",    TypeState::Error},
+    {"Allocated", TypeState::Allocated},
+    {"Freed",     TypeState::Freed},
+    {"Opened",    TypeState::Opened}
+};
+
+Map<TypeState, std::string> TypeStateParser::_revTypeMap = {
+    {TypeState::Uinit, "$uninit"},
+    {TypeState::Error, "$error"},
+    {TypeState::Allocated, "Allocated"},
+    {TypeState::Freed, "Freed"},
+    {TypeState::Opened, "Opened"}
+};
 
 SymState::SymState(ConsExeState es, TypeState ts) : _exeState(SVFUtil::move(es)), _typeState(SVFUtil::move(ts)),
     _branchCondition(Z3Expr::getContext().bool_val(true))
